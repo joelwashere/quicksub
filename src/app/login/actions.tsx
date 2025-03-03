8947cab61391dd56de9b649abad2a69e2dcaaf51
Joel Williams <joel@reboundstudios.com>
2025-03-03 03:31:18 -0500

    okey

---
+++ src/app/login/actions.tsx
@@ -0,0 +1,35 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { createClient } from '@/utils/supabase/server'
+
+export async function signIn() {
+  const supabase = await createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  //const data = {
+  //  email: formData.get('email') as string,
+  //  password: formData.get('password') as string,
+  //}
+
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      queryParams: {
+        prompt: "consent"
+      },
+      redirectTo: "http://quickbase.app/auth/callback",
+    },
+  })
+
+  if(error) {
+    redirect('/error')
+  } else if (data.url) {
+    redirect(data.url)
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
