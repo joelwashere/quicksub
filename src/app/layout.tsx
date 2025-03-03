325302f4bc3db644a2fdd4d4f0afdc167a6a96e1
Joel Williams <joel@reboundstudios.com>
2025-03-03 03:24:54 -0500

    trying to get this to build again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
-import SignInDialog from "@/components/sign-in-dialog";
 
 const opensans = Open_Sans({subsets: ["latin"]});
 
